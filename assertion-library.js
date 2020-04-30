ecab74d537696eca4458b9814768bf70e91d5711
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-04-30 13:55:26 +0200

    encapsulated and isolated tests - fail and success, test function

--- assertion-library.js
+++ assertion-library.js
@@ -2,13 +2,29 @@ const { sum, subtract } = require('./math')
 
 let result, expected
 
-result = sum(3, 7)
-expected = 10
-expect(result).toBe(expected)
+function sumTest() {
+	result = sum(3, 7)
+	expected = 10
+	expect(result).toBe(expected)
+}
+test('sum adds numbers', sumTest)
 
-result = subtract(7, 3)
-expected = 4
-expect(result).toBe(expected)
+function subtractTest() {
+	result = subtract(7, 3)
+	expected = 4
+	expect(result).toBe(expected)
+}
+test('subtract subtracts numbers', subtractTest)
+
+function test(title, cb) {
+	try {
+		cb()
+		console.log(`SUCCESS ${title}`)
+	} catch (error) {
+		console.error(`FAIL ${title}`)
+		console.error(error)
+	}
+}
 
 function expect(actual) {
 	return {
