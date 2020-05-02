a4dfaf46b996cdc2e5c15f7fe66235d9b24eaa93
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-05-02 19:24:15 +0200

    own implementation of jest.fn

---
+++ mocking/src/no-framework/mock-fn.js
@@ -0,0 +1,29 @@
+const assert = require('assert')
+const thumbWar = require('../thumb-war')
+const utils = require('../utils')
+
+function fn(impl) {
+	const mockFn = (...args) => {
+		mockFn.mock.calls.push(args)
+		return impl(...args)
+	}
+	mockFn.mock = { calls: [] }
+	return mockFn
+}
+
+const originalGetWinner = utils.getWinner
+utils.getWinner = fn((p1, p2) => p1)
+
+const winner = thumbWar('Uncle Bob', 'Uncle Sam')
+assert.strictEqual(winner, 'Uncle Bob')
+console.log(utils.getWinner.mock.calls)
+assert.deepStrictEqual(
+	utils.getWinner.mock.calls,
+	[
+		['Uncle Bob', 'Uncle Sam'],
+		['Uncle Bob', 'Uncle Sam'],
+	]
+)
+
+//cleanup
+utils.getWinner = originalGetWinner
