fc2eba670607c45bc6c2cd8769b8f38f8b85021c
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-05-02 12:19:37 +0200

    added kinda mock in monkey-patching

--- mocking/src/no-framework/monkey-patching.js
+++ mocking/src/no-framework/monkey-patching.js
@@ -2,5 +2,7 @@ const assert = require('assert')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
+utils.getWinner = (p1, p2) => p1
+
 const winner = thumbWar('Uncle Bob', 'Uncle Sam')
 assert.strictEqual(winner, 'Uncle Bob')
