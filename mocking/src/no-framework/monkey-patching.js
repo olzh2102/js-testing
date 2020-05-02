f1b443a90a13d9a586589a12b9051da97b98351e
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-05-02 12:20:46 +0200

    saved original fn functionality and returned back after test ran

--- mocking/src/no-framework/monkey-patching.js
+++ mocking/src/no-framework/monkey-patching.js
@@ -2,7 +2,10 @@ const assert = require('assert')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
+const originalGetWinner = utils.getWinner
 utils.getWinner = (p1, p2) => p1
 
 const winner = thumbWar('Uncle Bob', 'Uncle Sam')
 assert.strictEqual(winner, 'Uncle Bob')
+
+utils.getWinner = originalGetWinner
