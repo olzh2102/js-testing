777a2509802b5fa8a4fef907d9a5d17e33c35d7d
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-05-04 16:40:34 +0200

    getting within from dom testing library to cleanup unit test for favorite number

--- react-components/src/__tests__/favorite-number.js
+++ react-components/src/__tests__/favorite-number.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {queries} from '@testing-library/dom'
+import {within} from '@testing-library/dom'
 import {FavoriteNumber} from '../favorite-number'
 
 test('renders a number input with a label "Favoutire Number"', () => {
     const div = document.createElement('div')
     ReactDOM.render(<FavoriteNumber />, div)
+    const {getByLabelText} = within(div)
 
-    const input = queries.getByLabelText(div, /favorite Number/i) // will search inside div label with name of Favorite Number
+    const input = getByLabelText(/favorite Number/i) // will search inside div label with name of Favorite Number
 
     expect(input).toHaveAttribute('type', 'number')
 })
