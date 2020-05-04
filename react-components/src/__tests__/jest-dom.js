9cbb0e7b022089eefd442142241009df9f5d00c3
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-05-04 16:10:59 +0200

    extending expect functionality with jest-dom library

---
+++ react-components/src/__tests__/jest-dom.js
@@ -0,0 +1,14 @@
+import {toHaveAttribute, toHaveTextContent} from '@testing-library/jest-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {FavoriteNumber} from '../favorite-number'
+
+expect.extend({toHaveAttribute, toHaveTextContent})
+
+test('renders a number input with a label "Favoutire Number"', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<FavoriteNumber />, div)
+
+    expect(div.querySelector('input')).toHaveAttribute('type', 'number')
+    expect(div.querySelector('label')).toHaveTextContent('Favorite Number')
+})
