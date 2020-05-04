949f743064e1b5466e2b7a887f87558efad769de
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-05-04 17:58:08 +0200

    debug DOM state during test

--- react-components/src/__tests__/react-testing-library.js
+++ react-components/src/__tests__/react-testing-library.js
@@ -3,8 +3,9 @@ import {render} from '@testing-library/react'
 import {FavoriteNumber} from '../favorite-number'
 
 test('renders a number input with a label "Favoutire Number"', () => {
-    const {getByLabelText} = render(<FavoriteNumber />)
+    const {getByLabelText, debug} = render(<FavoriteNumber />)
     const input = getByLabelText(/favorite Number/i) // will search inside div label with name of Favorite Number
 
     expect(input).toHaveAttribute('type', 'number')
+    debug(input) // can be without parameter
 })
