8805c60aeb3d456162f20160e06d16242bc9219e
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-05-04 17:34:44 +0200

    removed render fucntion cause it is already coming with react-testing-library package

--- react-components/src/__tests__/react-testing-library.js
+++ react-components/src/__tests__/react-testing-library.js
@@ -1,18 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import {within} from '@testing-library/dom'
+import {render} from '@testing-library/react'
 import {FavoriteNumber} from '../favorite-number'
-
-function render(ui) {
-    const container = document.createElement('div')
-    ReactDOM.render(ui, container)
-    const queries = within(container)
-
-    return {
-        container,
-        ...queries,
-    }
-}
 
 test('renders a number input with a label "Favoutire Number"', () => {
     const {getByLabelText} = render(<FavoriteNumber />)
