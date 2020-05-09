6b26faf2f487e126945476a927e801bdb0ee7783
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-05-09 08:16:44 +0200

    Error boundary test - 1

--- react-components/src/__tests__/error-boundary-01.js
+++ react-components/src/__tests__/error-boundary-01.js
@@ -1,41 +1,33 @@
 import React from 'react'
-import {render, fireEvent} from '@testing-library/react'
+import {render} from '@testing-library/react'
 import {reportError as mockReportError} from '../api'
 import {ErrorBoundary} from '../error-boundary'
 
 jest.mock('../api')
 
-beforeAll(() => {
+// this is here to make the error output not appear in the project's output
+beforeEach(() => {
     jest.spyOn(console, 'error').mockImplementation(() => {})
 })
 
-afterAll(() => {
+afterEach(() => {
     console.error.mockRestore()
 })
 
-afterEach(() => {
-    jest.clearAllMocks()
-})
-
 function Bomb({shouldThrow}) {
-    if (shouldThrow) {
-        throw new Error('bomb')
-    } else {
-        return null
-    }
+    if (shouldThrow) throw new Error('bomb')
+    return null
 }
 
 test('calls reportError and renders that there was a problem', () => {
     mockReportError.mockResolvedValueOnce({success: true})
 
-    // == NO ERROR CASE ==
-    const {rerender, getByText, getByRole, queryByRole, queryByText} = render(
+    const {rerender} = render(
         <ErrorBoundary>
             <Bomb />
         </ErrorBoundary>,
     )
 
-    // == ERROR CASE ==
     rerender(
         <ErrorBoundary>
             <Bomb shouldThrow />
@@ -44,30 +36,7 @@ test('calls reportError and renders that there was a problem', () => {
 
     const error = expect.any(Error)
     const info = {componentStack: expect.stringContaining('Bomb')}
+
     expect(mockReportError).toHaveBeenCalledWith(error, info)
     expect(mockReportError).toHaveBeenCalledTimes(1)
-
-    // 2 times - once by jsdom and once by react
-    expect(console.error).toHaveBeenCalledTimes(2)
-
-    expect(getByRole('alert').textContent).toMatchInlineSnapshot(
-        `"There was a problem."`,
-    )
-
-    console.error.mockClear()
-    mockReportError.mockClear()
-
-    // == ERROR + TRY AGAIN BUTTON CASE ==
-    rerender(
-        <ErrorBoundary>
-            <Bomb />
-        </ErrorBoundary>,
-    )
-
-    fireEvent.click(getByText(/try again/i))
-
-    expect(mockReportError).not.toHaveBeenCalled()
-    expect(console.error).not.toHaveBeenCalled()
-    expect(queryByRole('alert')).not.toBeInTheDocument()
-    expect(queryByText(/try again/i)).not.toBeInTheDocument()
 })
